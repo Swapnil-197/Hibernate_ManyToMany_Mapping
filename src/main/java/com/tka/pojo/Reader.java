20fa3285f9a745267030d67c5db3174cc242d452
Swapnil-197 <sspatil1907@gmail.com>
2024-03-06 22:02:01 +0530

    Reader class updated

--- src/main/java/com/tka/pojo/Reader.java
+++ src/main/java/com/tka/pojo/Reader.java
@@ -1,5 +1,67 @@
 package com.tka.pojo;
 
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Reader {
+    @Id
+    @GeneratedValue
+	private Integer readerId;
+	public Set<Subscription> getSubscriptions() {
+		return subscriptions;
+	}
+
+	public void setSubscriptions(Set<Subscription> subscriptions) {
+		this.subscriptions = subscriptions;
+	}
+
+	private String email;
+	private String firstName;
+	private String lastName;
+	
+	private Set<Subscription> subscriptions;
+	
+	public Reader() {
+		super();
+	}
+
+	public Integer getReaderId() {
+		return readerId;
+	}
+
+	public void setReaderId(Integer readerId) {
+		this.readerId = readerId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
 
 }
